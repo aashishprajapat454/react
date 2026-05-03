2f4bcb48882a2152417f6d2e7b7a4dcf718cb608
Aashish Prajapat <aashishprajapat454@gmail.com>
2026-05-03 21:21:19 +0530

    first commit

---
+++ react/src/Comps/Navbar.jsx
@@ -0,0 +1,13 @@
+function Navbar(){
+    return(
+        <>
+            <ul>
+                <li><a href="home">Home</a></li>
+                <li><a href="About">About</a></li>
+                <li><a href="service">service</a></li>
+                <li><a href="Contact">Contact</a></li>
+            </ul>
+        </>
+    )
+}
+export default Navbar
